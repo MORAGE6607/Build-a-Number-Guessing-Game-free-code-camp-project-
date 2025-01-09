0493973c10daabf88c23d6c6b2097768b6b4d429
MORAGE6607 MORAGE6607 <morageluqman@gmail.com>
2025-01-09 07:38:04 +0000

    feat: add database table creation for users

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# PSQL variable
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Create the users table if it doesn't exist
+$PSQL "CREATE TABLE IF NOT EXISTS users (username VARCHAR(22) PRIMARY KEY, games_played INT DEFAULT 0, best_game INT DEFAULT NULL);"
