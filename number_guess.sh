755d318ec7e8dbf1596d78091a07a069f083cd86
MORAGE6607 MORAGE6607 <morageluqman@gmail.com>
2025-01-09 11:11:38 +0000

    feat:debuging the code

--- number_guess.sh
+++ number_guess.sh
@@ -22,7 +22,6 @@ USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$
 if [[ -z $USER_INFO ]]; then
   # New user
   echo "Welcome, $USERNAME! It looks like this is your first time here."
-  GAMES_PLAYED=0
   $PSQL "INSERT INTO users (username, games_played) VALUES ('$USERNAME', 0);"
 else
   # Existing user
@@ -43,33 +42,27 @@ fi
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 NUMBER_OF_GUESSES=0
 
-# Start the guessing game
 echo "Guess the secret number between 1 and 1000:"
 while true; do
   read GUESS
 
-  # Validate that the input is an integer
   if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
     echo "That is not an integer, guess again:"
     continue
   fi
 
-  # Increment the guess count
   NUMBER_OF_GUESSES=$(( NUMBER_OF_GUESSES + 1 ))
 
-  # Check the guess
   if (( GUESS < SECRET_NUMBER )); then
     echo "It's higher than that, guess again:"
   elif (( GUESS > SECRET_NUMBER )); then
     echo "It's lower than that, guess again:"
   else
-    # Correct guess
     echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
     break
   fi
 done
 
-# Update user statistics in the database
 NEW_GAMES_PLAYED=$(( GAMES_PLAYED + 1 ))
 
 if [[ -z $BEST_GAME ]] || [[ $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
