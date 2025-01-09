55b6377eef9d9b822f4b70c3067c5593f013320d
MORAGE6607 MORAGE6607 <morageluqman@gmail.com>
2025-01-09 07:40:02 +0000

    feat: implement guessing loop with feedback

--- number_guess.sh
+++ number_guess.sh
@@ -32,3 +32,25 @@ fi
 # Generate a random number between 1 and 1000
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
 NUMBER_OF_GUESSES=0
+
+# Game loop
+while true; do
+    read -p "Guess the secret number between 1 and 1000: " GUESS
+
+    # Check if the input is an integer
+    if ! [[ $GUESS =~ ^[0-9]+$ ]]; then
+        echo "That is not an integer, guess again:"
+        continue
+    fi
+
+    NUMBER_OF_GUESSES=$(( NUMBER_OF_GUESSES + 1 ))
+
+    if (( GUESS < SECRET_NUMBER )); then
+        echo "It's higher than that, guess again:"
+    elif (( GUESS > SECRET_NUMBER )); then
+        echo "It's lower than that, guess again:"
+    else
+        echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+        break
+    fi
+done
